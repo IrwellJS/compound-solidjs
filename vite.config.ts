6913a5e88ad4c5d081657cb2ca5e600d68cd169d
edjfl <d@d>
2024-03-22 22:44:38 +0000

    split js so branch shaking works and removed story definitions being added to the output

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import {defineConfig} from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import dts from 'vite-plugin-dts';
 import {libInjectCss} from "vite-plugin-lib-inject-css";
-import { resolve } from 'path'
+import { extname, relative, resolve } from 'path'
+import { fileURLToPath } from 'node:url'
 // import devtools from 'solid-devtools/vite';
 
 import * as pj from './package.json'
+import {glob} from "glob";
 
 interface PackageJson {
     peerDependencies: {}
@@ -23,6 +25,7 @@ export default defineConfig({
         libInjectCss(),
         dts({
             include: ['lib/'],
+            exclude: ['**/*.stories.tsx'],
         }),
         solidPlugin(),
     ],
@@ -31,15 +34,29 @@ export default defineConfig({
     },
     build: {
         copyPublicDir: false,
-        target: 'esnext',
         lib: {
             entry: resolve(__dirname, 'lib/index.ts'),
-            name: 'Compound SolidJS',
-            formats: ['es', 'umd'],
-            fileName: (format) => `compound-solidjs.${format}.js`,
+            formats: ['es'],
         },
         rollupOptions: {
             external: [...Object.keys(packageJson.peerDependencies)],
+            input: Object.fromEntries(
+                glob.sync('lib/**/*.{ts,tsx}', { ignore: 'lib/**/*.stories.tsx'}).map(file => [
+                    // The name of the entry point
+                    // lib/nested/foo.ts becomes nested/foo
+                    relative(
+                        'lib',
+                        file.slice(0, file.length - extname(file).length)
+                    ),
+                    // The absolute path to the entry file
+                    // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+                    fileURLToPath(new URL(file, import.meta.url))
+                ])
+            ),
+            output: {
+                assetFileNames: 'assets/[name][extname]',
+                entryFileNames: '[name].js',
+            },
         },
     },
 });
