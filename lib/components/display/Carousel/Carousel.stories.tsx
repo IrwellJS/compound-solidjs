a0125a73413b38ed23405ef8d29371b440592293
Edward fletcher <e@e>
2024-07-06 17:10:44 +0100

    updated basic background text colour to ensure visability

--- lib/components/display/Carousel/Carousel.stories.tsx
+++ lib/components/display/Carousel/Carousel.stories.tsx
@@ -2,14 +2,22 @@ import type {Meta, StoryObj} from 'storybook-solidjs';
 import {Carousel, CarouselProps} from "./index";
 import React from "react";
 import {Button} from "../../inputs";
-import {BackgroundType, Fit, GradientBackground, Size, Variant} from "../../../types";
+import {Fit, GradientBackground, Size, Variant} from "../../../types";
 import {Ticket} from "../Ticket";
+import {Container} from "../../../layouts";
 
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/solid/writing-stories/introduction
 const meta: Meta<typeof Carousel> = {
     title: 'Components/Display/Carousel',
     component: Carousel,
     tags: ['autodocs'],
+    decorators: [
+        (Story) => (
+            <Container backgroundType={GradientBackground.Azure}>
+                <Story />
+            </Container>
+        ),
+    ],
 };
 
 export default meta;
@@ -21,13 +29,11 @@ const getStory = (props: Partial<CarouselProps>): Story => {
             title: 'My Carousel',
             children: [
                 <Ticket
-                    backgroundType={GradientBackground.Mojito}
                     header="Infinite Wordle"
                     content="Guess the five letter word. Play as many rounds as you like."
                     footer={<Button variant={Variant.Secondary} fit={Fit.Block} size={Size.Sm} onClick={() => {}}>Play Now</Button>}
                 />,
                 <Ticket
-                    backgroundType={GradientBackground.Mojito}
                     content="sdf"
                     footer={<Button variant={Variant.Primary} fit={Fit.Block} size={Size.Sm} onClick={() => {}}>Play Now</Button>}
                 />
