560f35f37cf1b588d02660d65952a4c014e9a3cf
Edward fletcher <e@e>
2024-07-06 13:34:54 +0100

    simplified ticket to see if that improves the issue

--- lib/components/display/Ticket/index.tsx
+++ lib/components/display/Ticket/index.tsx
@@ -1,15 +1,7 @@
 import {Component, JSXElement} from "solid-js";
-import {getBackgroundClass, PartialBackgroundProps} from "../../../partials/Background";
+import {PartialBackgroundProps} from "../../../partials/Background";
 
 import './styles.css';
-
-// This is a safe list of classes so that the tree shaking can run correctly on css
-const classSafeList = [
-    'ticket',
-    'ticket-header',
-    'ticket-content',
-    'ticket-footer',
-];
 
 export interface TicketProps extends PartialBackgroundProps {
     header?: string | JSXElement;
@@ -19,10 +11,12 @@ export interface TicketProps extends PartialBackgroundProps {
 
 export const Ticket: Component<TicketProps> = (props) => {
     return (
-        <div class={`ticket ${getBackgroundClass(props)}`}>
+        <div class="ticket">
             {props.header && <div class="ticket-header">{props.header}</div>}
             <div class="ticket-content">{props.content}</div>
             {props.footer && <div class="ticket-footer">{props.footer}</div>}
         </div>
     );
 }
+
+export default Ticket;
